c284724ca8c07ed310a7144b57670966ed726b4f
prashanth-acsq <prashanth@acsq.com.qa>
2022-09-01 16:00:39 +0300

    Initial

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ import numpy as np
 
 from openvino.runtime import Core
 
-RECOG_MODEL_PATH = os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), "models"), "recog_model.xml")
-DETECT_MODEL_PATH = os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), "models"), "detect_model.xml")
+MODEL_BASE_PATH: str = os.path.join(os.path.abspath(os.path.dirname(__file__)), "models")
+
+DETECT_MODEL_PATH: str  = os.path.join(MODEL_BASE_PATH, "detect_model.xml")
+RECOG_MODEL_PATH_1: str = os.path.join(MODEL_BASE_PATH, "recog_model_1.xml")
+RECOG_MODEL_PATH_2: str = os.path.join(MODEL_BASE_PATH, "recog_model_2.xml")
+RECOG_MODEL_PATH_3: str = os.path.join(MODEL_BASE_PATH, "recog_model_3.xml")
+
 IMAGE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "images")
 
 ID: int = 0
@@ -21,8 +26,11 @@ def breaker(num: int=50, char: str="*") -> None:
     print("\n" + num*char + "\n")
 
 
-def preprocess(image: np.ndarray, width: int, height: int) -> np.ndarray:
-    image = cv2.resize(src=image, dsize=(width, height), interpolation=cv2.INTER_AREA).transpose(2, 0, 1)
+def preprocess(image: np.ndarray, width: int, height: int, model_name: str="arcface") -> np.ndarray:
+    if model_name == "arcface" or model_name == "sphereface":
+        image = cv2.resize(src=image, dsize=(width, height), interpolation=cv2.INTER_AREA).transpose(2, 0, 1)
+    else:
+        image = cv2.resize(src=image, dsize=(width, height), interpolation=cv2.INTER_AREA)
     return np.expand_dims(image, axis=0)
 
 
@@ -77,21 +85,29 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--filename", "-f", type=str, default="Test_1.jpg", help="Image Filename")
     parser.add_argument("--target", "-t", type=str, default="CPU", help="Target Device for Inference")
+    parser.add_argument("--model", "-m", type=str, default="arcface", help="Model to Use (arcface, facenet, sphereface)")
     args = parser.parse_args()
 
     assert args.filename in os.listdir(IMAGE_PATH), "File not Found"
     assert args.target in ["CPU", "GPU"], "Invalid Target Device"
 
-    d_model, _, d_output_layer, (_, _, d_H, d_W) = setup(args.target, DETECT_MODEL_PATH)
-    r_model, _, r_output_layer, (_, _, r_H, r_W) = setup(args.target, RECOG_MODEL_PATH)
-
     image = cv2.imread(os.path.join(IMAGE_PATH, args.filename), cv2.IMREAD_COLOR)
     temp_image = image.copy()
     h, w, _ = image.shape
-    image = preprocess(image, d_W, d_H)
 
+    d_model, _, d_output_layer, (_, _, d_H, d_W) = setup(args.target, DETECT_MODEL_PATH)
+    
+    if args.model == "arcface":
+        r_model, _, r_output_layer, (_, _, r_H, r_W) = setup(args.target, RECOG_MODEL_PATH_1)
+    elif args.model == "facenet":
+        r_model, _, r_output_layer, (_, r_H, r_W, _) = setup(args.target, RECOG_MODEL_PATH_2)
+    elif args.model == "sphereface":
+        r_model, _, r_output_layer, (_, _, r_H, r_W) = setup(args.target, RECOG_MODEL_PATH_3)
+
+    image = preprocess(image, d_W, d_H)
     _, _, boxes = detect_faces(d_model, d_output_layer, image, w, h)
-    face_image = preprocess(temp_image[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :], r_W, r_H)
+
+    face_image = preprocess(temp_image[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :], r_W, r_H, args.model)
     reference_embeddings = r_model(inputs=[face_image])[r_output_layer]
 
     if platform.system() != "Windows":
@@ -115,7 +131,7 @@ def main():
             # print("ROI to small to detect")
             cv2.putText(disp_frame, "ROI to small to detect", org=(25, 75), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=1, color=(0, 0, 255))
         else:
-            face_frame = preprocess(face_frame, r_W, r_H)
+            face_frame = preprocess(face_frame, r_W, r_H, args.model)
             embeddings = r_model(inputs=[face_frame])[r_output_layer]
 
             cs = cosine_similarity(reference_embeddings, embeddings)[0][0]
@@ -130,8 +146,6 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
-
-    
 
 if __name__ == "__main__":
     sys.exit(main() or 0)
