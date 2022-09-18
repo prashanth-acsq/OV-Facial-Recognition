c12be152f8c0a5a31998f1c01e46f1f3a7cc2b4c
prashanth-acsq <prashanth@acsq.com.qa>
2022-09-18 10:38:50 +0300

    Added support for image/image comparisons + Update requirements.txt

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import cv2
 import platform
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 
+from typing import Union
 from openvino.runtime import Core
 
 MODEL_BASE_PATH: str = os.path.join(os.path.abspath(os.path.dirname(__file__)), "models")
@@ -60,7 +62,8 @@ def detect_faces(
     output_layer, 
     image: np.ndarray, 
     w: int, 
-    h: int) -> tuple:
+    h: int
+) -> tuple:
     """
         Detect faces in the image. Returns a tuple of label indexes, probabilities and bounding boxes. (Possibly switch to detect only single face)
     """
@@ -87,25 +90,54 @@ def detect_faces(
     return label_indexes, probs, boxes
 
 
+def show_images(
+    image_1: np.ndarray,
+    image_2: np.ndarray,
+    cmap_1: str="gnuplot2",
+    cmap_2: str="gnuplot2",
+    title_1: Union[str, None]=None,
+    title_2: Union[str, None]=None,
+    title: Union[str, None]=None,
+) -> None:
+    plt.figure()
+    plt.rcParams.update({"font.size" : 36})
+    plt.subplot(1, 2, 1)
+    plt.imshow(cv2.cvtColor(src=image_1, code=cv2.COLOR_BGR2RGB), cmap_1)
+    plt.axis("off")
+    if title_1: plt.title(title_1)
+    plt.subplot(1, 2, 2)
+    plt.imshow(cv2.cvtColor(src=image_2, code=cv2.COLOR_BGR2RGB), cmap_2)
+    plt.axis("off")
+    if title_2: plt.title(title_2)
+    figmanager = plt.get_current_fig_manager()
+    figmanager.window.state("zoomed")
+    if title: plt.suptitle(title)
+    plt.show()
+
+
 def main():
 
     # CLI Argument Parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filename", "-f", type=str, default="Test_1.jpg", help="Image Filename")
+    parser.add_argument("--mode", "-m", type=str, default="image", help="Image/Image or Image/Realtime")
+    parser.add_argument("--filename1", "-f1", type=str, default="Test_1.jpg", help="Image Filename 1")
+    parser.add_argument("--filename2", "-f2", type=str, default="Test_2.jpg", help="Image Filename 2")
     parser.add_argument("--target", "-t", type=str, default="CPU", help="Target Device for Inference")
-    parser.add_argument("--model", "-m", type=str, default="facenet", help="Model to Use (arcface, facenet, sphereface)")
+    parser.add_argument("--model", "-mo", type=str, default="facenet", help="Model to Use (arcface, facenet, sphereface)")
     args = parser.parse_args()
 
     # Checks for CLI arguments
-    assert args.filename in os.listdir(IMAGE_PATH), "File not Found"
+    assert args.mode == "image" or args.mode == "realtime", "Invalid Mode"
+    assert args.filename1 in os.listdir(IMAGE_PATH), "File 1 not Found"
     assert args.target in ["CPU", "GPU"], "Invalid Target Device"
     assert args.model == "arcface" or args.model == "facenet" or args.model == "sphereface", "Invalid Model"
 
     # Adaptive Equalization Setup
     clahe = cv2.createCLAHE(clipLimit=2.5, tileGridSize=(5, 5))
 
-    # Read Image and Apply CLAHE
-    image = cv2.imread(os.path.join(IMAGE_PATH, args.filename), cv2.IMREAD_COLOR)
+    # Read Reference Image and Apply CLAHE
+    image = cv2.imread(os.path.join(IMAGE_PATH, args.filename1), cv2.IMREAD_COLOR)
+    disp_image_1 = image.copy()
     for i in range(3):
         image[:, :, i] = clahe.apply(image[:, :, i])
     temp_image = image.copy()
@@ -127,66 +159,89 @@ def main():
     face_image = preprocess(temp_image[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :], r_W, r_H, args.model)
     reference_embeddings = r_model(inputs=[face_image])[r_output_layer]
 
-    # Initialize Video Capture Object
-    if platform.system() != "Windows": cap = cv2.VideoCapture(ID)
-    else: cap = cv2.VideoCapture(ID, cv2.CAP_DSHOW)
-    
-    # Set parameters of capture object
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
-    cap.set(cv2.CAP_PROP_FPS, FPS)
+    if args.mode == "image":
+        assert args.filename2 in os.listdir(IMAGE_PATH), "File 2 not Found"
 
-    # Read data from Video Capture Object
-    while True:
-        ret, frame = cap.read()
-        if not ret: break
-
-        # Make a copy for processing purposes
-        temp_frame = frame.copy()
-
-        # Make a copy for display purposes
-        disp_frame = frame.copy()
-
-        # Apply CLAHE
+        comp_image = cv2.imread(os.path.join(IMAGE_PATH, args.filename2), cv2.IMREAD_COLOR)
+        disp_image_2 = comp_image.copy()
         for i in range(3):
-            frame[:, :, i] = clahe.apply(frame[:, :, i])
-            temp_frame[:, :, i] = clahe.apply(temp_frame[:, :, i])
+            comp_image[:, :, i] = clahe.apply(comp_image[:, :, i])
+        temp_image = comp_image.copy()
+        h, w, _ = comp_image.shape
 
-            # # CLAHE Display Frame (Test Purposes)
-            # disp_frame[:, :, i] = clahe.apply(disp_frame[:, :, i])
+        comp_image = preprocess(comp_image, d_W, d_H)
+        _, _, boxes = detect_faces(d_model, d_output_layer, comp_image, w, h)
 
-        # Preprocess Frame and Detect Faces
-        frame = preprocess(frame, d_W, d_H)
-        _, _, boxes = detect_faces(d_model, d_output_layer, frame, CAM_WIDTH, CAM_HEIGHT)
+        face_image = preprocess(temp_image[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :], r_W, r_H, args.model)
 
-        # Preprocess face ROI Frame and get embeddings
-        face_frame = temp_frame[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :]
+        # if face_image.shape[0] < 32 or face_image.shape[1] < 32: print("ROI to small to detect")
+        # else:
+        embeddings = r_model(inputs=[face_image])[r_output_layer]
+        cs = cosine_similarity(reference_embeddings, embeddings)[0][0]
+        show_images(disp_image_1, disp_image_2, title=f"Score : {cs:.2f}")
 
-        # If condition is met, compute Cosine Similarity between embeddings
-        if face_frame.shape[0] < 32 or face_frame.shape[1] < 32:
-            # print("ROI to small to detect")
-            cv2.putText(disp_frame, "ROI to small to detect", org=(25, 75), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=1, color=(0, 0, 255))
-        else:
-            face_frame = preprocess(face_frame, r_W, r_H, args.model)
-            embeddings = r_model(inputs=[face_frame])[r_output_layer]
 
-            cs = cosine_similarity(reference_embeddings, embeddings)[0][0]
-
-            # print(f"{cs:.2f}")
-            cv2.putText(disp_frame, f"{cs:.2f}", org=(25, 75), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=1, color=(0, 255, 0))
+    elif args.mode == "realtime":
+        # Initialize Video Capture Object
+        if platform.system() != "Windows": cap = cv2.VideoCapture(ID)
+        else: cap = cv2.VideoCapture(ID, cv2.CAP_DSHOW)
         
-        # Display the frame
-        cv2.imshow("Feed", disp_frame)
+        # Set parameters of capture object
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+        cap.set(cv2.CAP_PROP_FPS, FPS)
 
-        # Press 'q' to Quit
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
-            break
-    
-    # Release the Video Capture Object
-    cap.release()
+        # Read data from Video Capture Object
+        while True:
+            ret, frame = cap.read()
+            if not ret: break
 
-    # Destroy all cv2 Windows
-    cv2.destroyAllWindows()
+            # Make a copy for processing purposes
+            temp_frame = frame.copy()
+
+            # Make a copy for display purposes
+            disp_frame = frame.copy()
+
+            # Apply CLAHE
+            for i in range(3):
+                frame[:, :, i] = clahe.apply(frame[:, :, i])
+                temp_frame[:, :, i] = clahe.apply(temp_frame[:, :, i])
+
+                # # CLAHE Display Frame (Test Purposes)
+                # disp_frame[:, :, i] = clahe.apply(disp_frame[:, :, i])
+
+            # Preprocess Frame and Detect Faces
+            frame = preprocess(frame, d_W, d_H)
+            _, _, boxes = detect_faces(d_model, d_output_layer, frame, CAM_WIDTH, CAM_HEIGHT)
+
+            # Preprocess face ROI Frame and get embeddings
+            face_frame = temp_frame[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :]
+
+            # If condition is met, compute Cosine Similarity between embeddings
+            if face_frame.shape[0] < 32 or face_frame.shape[1] < 32:
+                # print("ROI to small to detect")
+                cv2.putText(disp_frame, "ROI to small to detect", org=(25, 75), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=1, color=(0, 0, 255))
+            else:
+                face_frame = preprocess(face_frame, r_W, r_H, args.model)
+                embeddings = r_model(inputs=[face_frame])[r_output_layer]
+
+                cs = cosine_similarity(reference_embeddings, embeddings)[0][0]
+
+                # print(f"{cs:.2f}")
+                cv2.putText(disp_frame, f"{cs:.2f}", org=(25, 75), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=1, color=(0, 255, 0))
+            
+            # Display the frame
+            cv2.imshow("Feed", disp_frame)
+
+            # Press 'q' to Quit
+            if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break
+        
+        # Release the Video Capture Object
+        cap.release()
+
+        # Destroy all cv2 Windows
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
